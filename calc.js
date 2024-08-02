9304ec6c6dbb7ad4ddb193f69edee81b49bcd24c
Jonathan Moody <jonathanmoody15@gmail.com>
2024-08-01 21:44:38 -0400

    calc.html: added div and button elements for layout of calculator
    style.css: styled elements in calc.html
    
    calc.js: initalized 3 empty string variables and linked 3 const variables to 3 html elements
    
    to-do:
    1.round edges on calculator in css file
    2.link the rest of the html elements to const variables
    3.create simple functions to handle basic mathematic operations

---
+++ calc.js
@@ -0,0 +1,8 @@
+let currentNum = "";
+let prevNum = "";
+let operator = "";
+
+const display = document.querySelector("#display");
+const clear = document.querySelector(".clear");
+const digits = document.querySelectorAll(".digit");
+console.log(digits);
