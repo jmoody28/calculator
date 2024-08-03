48ab421c36934cb8ed3e1c3592e03c1fca0aa553
Jonathan Moody <jonathanmoody15@gmail.com>
2024-08-02 23:58:40 -0400

    calc.html: added "text" class to p element in "display" div
    calc.js:
    1. correctly linked decimal element to decimal const variable
    2. added console.log statements to getNumber and getOperator functions
    3. added an event listener to equal variable
    4. created operate function
    
    to-do:
    1. round edges on calculator in css file
    2. create function to evaluate decimals
    3. add more statements to operate function

--- calc.js
+++ calc.js
@@ -2,7 +2,60 @@ let currentNum = "";
 let prevNum = "";
 let operator = "";
 
-const display = document.querySelector("#display");
+const text = document.querySelector("#text");
 const clear = document.querySelector(".clear");
 const digits = document.querySelectorAll(".digit");
-console.log(digits);
+const operators = document.querySelectorAll(".operator");
+const decimal = document.querySelectorAll(".decimal");
+const equal = document.querySelector(".equal");
+
+digits.forEach((digit) =>
+  digit.addEventListener("click", function (d) {
+    getNumber(d.target.textContent);
+    text.textContent = currentNum;
+  })
+);
+
+function getNumber(num) {
+  if (currentNum.length <= 7) {
+    currentNum += num;
+    console.log(prevNum, operator, currentNum);
+  }
+}
+
+operators.forEach((op) =>
+  op.addEventListener("click", function (o) {
+    getOperator(o.target.value);
+    text.textContent = o.target.textContent;
+  })
+);
+
+function getOperator(symbol) {
+  if (currentNum != "" && prevNum == "") {
+    prevNum = currentNum;
+    currentNum = "";
+    operator += symbol;
+    console.log(currentNum, prevNum, operator);
+  } else if (s) {
+  }
+}
+
+equal.addEventListener("click", function () {
+  operate(prevNum, operator, currentNum);
+});
+
+clear.addEventListener("click", function () {
+  currentNum = "";
+  prevNum = "";
+  operator = "";
+  text.textContent = "0";
+});
+
+function operate(num1, op, num2) {
+  if (op == "add") {
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    sum = num1 + num2;
+    return (text.textContent = sum.toString());
+  }
+}
